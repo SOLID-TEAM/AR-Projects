608e80806d8397a4e57c2c339b72a694db0c3dbc
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-03-27 22:27:54 +0100

    updated scale factor method

--- template_matching.py
+++ template_matching.py
@@ -1,30 +1,28 @@
 import numpy as np
 import cv2
 
-# input_img_path = input('Input image: ')
-# input_img_trg_path = input('Target image: ')
-# input_threshold = input('Detection threshold(float): ')
+input_img_path = input('Input image: ')
+input_img_trg_path = input('Target image: ')
+input_threshold = input('Detection threshold(0.1): ')
+scale_multipler = float(input('Scale multiplier(1 or 0.5): '))
 
+img_org = cv2.imread(input_img_path, 1)
+img_temp = cv2.imread(input_img_path, 0) / 255.0
+img_target = cv2.imread(input_img_trg_path, 0) / 255.0
 
-# img_org = cv2.imread(input_img_path, 1)
-# img_temp = cv2.imread(input_img_path, 0) / 255.0
-# img_target = cv2.imread(input_img_trg_path, 0) / 255.0
-
-input_threshold = 0.1
-
-img_org = cv2.imread('img2.png', 1)
-img_temp = cv2.imread('img2.png', 0) / 255.0
-img_target = cv2.imread('t1-img2.png', 0) / 255.0
+# img_org = cv2.imread('img2.png', 1)
+# img_temp = cv2.imread('img2.png', 0) / 255.0
+# img_target = cv2.imread('t1-img2.png', 0) / 255.0
 
 threshold = float(input_threshold)
 
-img_temp = cv2.resize(img_temp, (int(img_org.shape[1] * 0.5), int(img_org.shape[0] * 0.5)),  interpolation=cv2.INTER_LINEAR)
-img_target = cv2.resize(img_target, (int(img_target.shape[1] *0.5), int(img_target.shape[0] * 0.5)), interpolation=cv2.INTER_LINEAR)
+img_temp = cv2.resize(img_temp, (int(img_org.shape[1] * scale_multipler), int(img_org.shape[0] * scale_multipler)),  interpolation=cv2.INTER_NEAREST)
+img_target = cv2.resize(img_target, (int(img_target.shape[1] * scale_multipler), int(img_target.shape[0] * scale_multipler)), interpolation=cv2.INTER_NEAREST)
 
 h_org, w_org = img_temp.shape
 h_trg, w_trg = img_target.shape
 
-img_calc = np.zeros((h_org - h_trg + 1, w_org - w_trg + 1)) 
+img_calc = np.zeros((h_org - h_trg + 1, w_org - w_trg + 1))
 
 for y in range(0, h_org - h_trg + 1):
     for x in range(0, w_org - w_trg + 1):
@@ -34,7 +32,7 @@ img_calc = img_calc / img_calc.max()
 locations = np.where(img_calc <= threshold)
 
 for pt in zip(*locations[::-1]):
-    cv2.rectangle(img_org, (pt[0]*2 , pt[1]*2), (pt[0]*2 + h_trg*2, pt[1]*2 + w_trg*2), (0, 255, 0), 1)
+    cv2.rectangle(img_org, (int(pt[0] / scale_multipler), int(pt[1] / scale_multipler)), (int(pt[0] / scale_multipler + h_trg / scale_multipler), int(pt[1] / scale_multipler + w_trg / scale_multipler)), (0, 255, 0), 1)
 
 cv2.imshow('Result', img_org)
 cv2.imshow('matching map', img_calc)
