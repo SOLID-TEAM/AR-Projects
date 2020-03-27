9779a29aaf7570dbada4e6f3fa166c60ab42054d
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-03-27 21:30:23 +0100

    showing all required images

--- template_matching.py
+++ template_matching.py
@@ -1,11 +1,24 @@
 import numpy as np
 import cv2
 
-img_org = cv2.imread("img2.png", 1)
-img_temp = cv2.imread("img2.png", 0) / 255.0
-img_target = cv2.imread("t1-img2.png", 0) / 255.0
+# input_img_path = input('Input image: ')
+# input_img_trg_path = input('Target image: ')
+# input_threshold = input('Detection threshold(float): ')
 
-threshold = 0.1
+
+# img_org = cv2.imread(input_img_path, 1)
+# img_temp = cv2.imread(input_img_path, 0) / 255.0
+# img_target = cv2.imread(input_img_trg_path, 0) / 255.0
+
+input_threshold = 0.1
+
+img_org = cv2.imread('img2.png', 1)
+img_temp = cv2.imread('img2.png', 0) / 255.0
+img_target = cv2.imread('t1-img2.png', 0) / 255.0
+
+#cv2.resize()
+
+threshold = float(input_threshold)
 
 h_org, w_org = img_temp.shape
 h_trg, w_trg = img_target.shape
@@ -22,9 +35,20 @@ locations = np.where(img_calc <= threshold)
 for pt in zip(*locations[::-1]):
     cv2.rectangle(img_org, pt, (pt[0] + h_trg, pt[1] + w_trg), (0, 255, 0), 1)
 
+cv2.imshow('Result', img_org)
+cv2.imshow('matching map', img_calc)
+cv2.imshow('Target image', img_target)
 
+print(len(locations))
 
-cv2.imshow('test', img_org)
+imgFound = np.zeros((40,320,3), np.uint8)
+font = cv2.FONT_HERSHEY_SIMPLEX
+if (len(locations) > 0):
+    cv2.putText(imgFound,'TARGET FOUND', (5,30), font, 1, (0,255,0),2)
+else:
+    cv2.putText(imgFound,'TARGET NOT FOUND', (5,30), font, 1, (0,255,0),2)
+
+cv2.imshow('found', imgFound)
 
 k = cv2.waitKey(0)
 
